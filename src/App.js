24e070f36c68c911f34b74ced2e08dbcbbc4f468
BoXi9252 <zhaoyanglove2022@126.com>
2024-05-11 00:40:02 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import StarRating from "./components/StarRating";
+import Dialog from "./components/Dialog";
+import reactLogo from "./assets/images/logo.png";
+
+
+function App() {
+  const [isShowDialog, setIsShowDialog] = React.useState(false);
+  return (
+    <div className="App">
+      <div className="container">
+        <StarRating />
+        <Dialog
+          visible={isShowDialog}
+          onCancel={() => {
+            setIsShowDialog(false);
+          }}
+        >
+          <div className="dialog-children">
+            <img src={reactLogo} alt="" />
+            <button onClick={() =>{
+              setIsShowDialog(false);
+            }}> Close Dialog</button>
+          </div>
+        </Dialog>
+        <div className="button-wrapper">
+          <button onClick={() => {
+            setIsShowDialog(true);
+          }}>Open Dialog</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,38 @@
+const Constants = {
+  /** 展示星星的信息 */
+  starInfo: [
+    {
+      score: 1,
+      text: "Not good",
+    },
+    {
+      score: 2,
+      text: "Acceptable",
+    },
+    {
+      score: 3,
+      text: "Fine",
+    },
+    {
+      score: 4,
+      text: "Good",
+    },
+    {
+      score: 5,
+      text: "Great",
+    },
+  ],
+  /** 未评分的星星信息 */
+  defStar: {
+    score: 0,
+    text: "Unknown"
+  },
+  /** 星星的大小 */
+  starSize: 30,
+  /** 激活的星颜色 */
+  actColor: "#ff6",
+  /** 未激活的星颜色 */
+  defColor: "#fff",
+};
+
+export default Constants;
